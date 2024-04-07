f649600783272868c99e0c34170910b2ffac2a8c
fadeyush <a.fadeyush@gmail.com>
2024-04-07 19:09:22 +0300

    fix

--- src/components/productCatalogItem/ProductCatalogItem.tsx
+++ src/components/productCatalogItem/ProductCatalogItem.tsx
@@ -2,15 +2,16 @@ import React, { FC } from 'react';
 import classes from './ProductCatalogItem.module.scss'
 import { ProductState } from '../../types/products';
 import { useNavigate } from 'react-router-dom';
+import MyButton from '../UI/button/MyButton';
 
 const ProductCatalogItem: FC<ProductState> = ({id, image, title, price}) => {
-    
     const router = useNavigate();
     return (
-        <li onClick={()=>router(`/products/${id}`)} className={classes.productCatalogItem}>
+        <li className={classes.productCatalogItem}>
             <img width='100px' height='100px' src={image} alt='Картинка продукта'></img>
             <h3 className={classes.productCatalogItem__title}>{title}</h3>
             <p className={classes.productCatalogItem__price}>Цена: {price}</p>
+            <MyButton onClick={()=>router(`/products/${id}`)}>Открыть</MyButton>
         </li>
     );
 };
