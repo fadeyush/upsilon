e5164836fb309a889d5522fe545952aed02d5e9b
fadeyush <a.fadeyush@gmail.com>
2024-04-09 21:19:15 +0300

    поиск в списке продуктов

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -1,21 +1,30 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
 import classes from './ProductList.module.scss'
 import { useAppSelector } from '../../hooks/redux';
 
 const ProductList: FC = () => {
     const {productsList, isLoading, error} = useAppSelector(state => state.productListReducer);
+    const [searchQuery, setSearchQuery] = useState<string>('');
+
+    const searchPosts = useMemo(()=>{
+        return productsList.filter(product=> product.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    }, [searchQuery, productsList]);
     
     return (
-        <ul className={classes.productList}>
-            {isLoading ?  
-                <h2>Загрузка...</h2> : 
-            error ? 
-                <h2>{error}</h2> 
-            :
-            productsList.map(product =>
-                <li key={product.id}>{product.id} - {product.title}</li>
-            )}
-        </ul>
+        <>
+            <input className={classes.productList__search} placeholder='Поиск...' value = {searchQuery} onChange={e => setSearchQuery(e.target.value)}
+            type='text'></input>
+            <ul className={classes.productList}>
+                {isLoading ?  
+                    <h2>Загрузка...</h2> : 
+                error ? 
+                    <h2>{error}</h2> 
+                :
+                searchPosts.map(product =>
+                    <li key={product.id}>{product.id} - {product.title}</li>
+                )}
+            </ul>
+        </>
     );
 };
 
