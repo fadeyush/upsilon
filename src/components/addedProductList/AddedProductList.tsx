c6e5dd36053ef6eda1e716a5e332b65d53ea11b8
fadeyush <a.fadeyush@gmail.com>
2024-04-09 14:26:39 +0300

    фильтрация списка продуктов с переключателем switch

--- src/components/addedProductList/AddedProductList.tsx
+++ src/components/addedProductList/AddedProductList.tsx
@@ -1,33 +1,60 @@
 import React, { FC, useState } from 'react';
 import classes from './AddedProductList.module.scss';
+import Switch from '@mui/material/Switch';
 import { AddProductProps } from '../../types/addproduct';
 
 const AddedProductList: FC = () => {
     const addedProductList: AddProductProps[] = JSON.parse(localStorage.getItem('addedProductList')!) || [];
+    const [checked, setChecked] = React.useState(false);
 
-    const columnsName = [
-        {Header: 'Дата создание',accessor: 'date',},{Header: 'Название',accessor: 'title',},{ Header: 'Цена',accessor: 'price',},{Header: 'Описание',accessor: 'description',},{Header: 'Опубликовано',accessor: 'isPublished'}];
+    const publishedaddedProductList: AddProductProps[] = addedProductList.filter(product=>product.isAddProductPublished === true);
+    const notPublishedaddedProductList: AddProductProps[] = addedProductList.filter(product=>product.isAddProductPublished === false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
+
+    const columnsName = [{Header: 'Дата создание',accessor: 'date',},{Header: 'Название',accessor: 'title',},{ Header: 'Цена',accessor: 'price',},{Header: 'Описание',accessor: 'description',},{Header: 'Опубликовано',accessor: 'isPublished'}];
+
     return (
-        <table className={classes.AddedProductList}>
-            <thead>
-                <tr>
-                    {columnsName.map(columnName=>
-                         <th className={classes.AddedProductList__head} key={columnName.accessor}>{columnName.Header}</th>
-                    )}
-                </tr>
-            </thead>
-            <tbody>
-                {addedProductList.map(product=>
-                    <tr id={product.idAddProduct} key={product.idAddProduct} className={classes.AddedProductList__info}>
-                         <td>{product.dateAddProduct}</td>
-                         <td>{product.titleAddProduct}</td>
-                         <td>{product.priceAddProduct}</td>
-                         <td>{product.descriptionAddProduct}</td>
-                         <td>{product.isAddProductPublished ? 'да' : 'нет'}</td>
+        <section className={classes.AddedProductList}>
+            <div className={classes.AddedProductList__toggleSwitch}>
+                <Switch checked={checked} onChange={handleChange} inputProps={{ 'aria-label': 'controlled' }} />
+            </div>
+            <table className={classes.AddedProductList__table}>
+                <thead>
+                    <tr>
+                        {columnsName.map(columnName=>
+                            <th className={classes.AddedProductList__head} key={columnName.accessor}>{columnName.Header}</th>
+                        )}
                     </tr>
-                )}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {checked ? 
+                    publishedaddedProductList.map(product=>
+                        <tr id={product.idAddProduct} key={product.idAddProduct} className={classes.AddedProductList__info}>
+                            <td className={classes.AddedProductList__date}>{product.dateAddProduct}</td>
+                            <td>{product.titleAddProduct}</td>
+                            <td>{product.priceAddProduct}</td>
+                            <td>{product.descriptionAddProduct}</td>
+                            <td>{product.isAddProductPublished ? 'да' : 'нет'}</td>
+                        </tr>
+                    )
+                     :
+                     notPublishedaddedProductList.map(product=>
+                        <tr id={product.idAddProduct} key={product.idAddProduct} className={classes.AddedProductList__info}>
+                            <td className={classes.AddedProductList__date}>{product.dateAddProduct}</td>
+                            <td>{product.titleAddProduct}</td>
+                            <td>{product.priceAddProduct}</td>
+                            <td>{product.descriptionAddProduct}</td>
+                            <td>{product.isAddProductPublished ? 'да' : 'нет'}</td>
+                        </tr>
+                     )
+                    }
+                </tbody>
+            </table>
+        </section>
+        
     );
 };
 
